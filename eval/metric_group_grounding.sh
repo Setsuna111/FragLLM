738a3874faf3ec55419b49db72d53fb2858cb3ff
Lay-du <2224123622@qq.com>
2025-09-11 13:58:43 +0800

    修改为通过向量检索的方式分匹配类别，传入保存的向量和类别列表

--- eval/metric_group_grounding.sh
+++ eval/metric_group_grounding.sh
@@ -3,9 +3,9 @@
 ## USAGE
 
 export PYTHONPATH="./:$PYTHONPATH"
-Results_Path=./eval_results/group_grounding/fragment_training_only_stage2_bw_stage1_lora32_epoch10_0901_merge/ActGroundGroup_results.csv
+Results_Path=./eval_results/group_grounding/fragment_training_only_stage2_bw_stage1_lora32_epoch10_0901_merge/BindIGroundGroup_results.csv
 IOU_Threshold=0.5
-BERT_Threshold=0.5
+BERT_Threshold=1.0
+Embedding_Path=/home/djy/projects/Documents/FragLLM_git/eval_local/vectors_val_test/VenusX_BindI.npz
 
-
-python eval_local/metric_group_grounding.py --results_path $Results_Path --iou_threshold $IOU_Threshold --bert_threshold $BERT_Threshold
+python eval_local/metric_group_grounding.py --results_path $Results_Path --iou_threshold $IOU_Threshold --bert_threshold $BERT_Threshold --embedding_path $Embedding_Path
